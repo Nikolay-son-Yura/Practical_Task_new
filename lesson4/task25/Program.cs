f934a5c18233d074ba90fb7b16f508f8a15fd9ea
Nikolay Viktorov <nikola_doma@mail.ru>
2022-11-13 13:32:16 +0700

    добавлене дз 4

---
+++ lesson4/task25/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Write("Введите  число А:  ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите  число В:  ");
+int numB = int.Parse(Console.ReadLine()!);
+int num = numA;
+int num1 = 1;
+while (num1 < numB)
+{
+    numA = numA * num;
+    num1++;
+}
+Console.WriteLine($"{num}^{numB}={numA}");
---
+++ lesson4/task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+        Console.Write(element);
+}
+Console.Write("Введите длину массива:  ");
+int num = int.Parse(Console.ReadLine()!);
+int num1 =1;
+int[] array = new int[num];
+while(num1<=num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {num1} элемент масива:  ");
+        array[i]=int.Parse(Console.ReadLine()!);
+        num1++;
+    }
+    //num1++;
+}
+
+
+System.Console.WriteLine("Ваш массив ");
+PrintArray(array);
